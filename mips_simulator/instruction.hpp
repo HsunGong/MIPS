99969c6a42cb64cf227191425cf9251969dcf6ee
Xun <gongxuncd@gmail.com>
2018-07-11 22:02:23 +0800

    finish v0.9

--- mips_simulator/instruction.hpp
+++ mips_simulator/instruction.hpp
@@ -13,4 +13,72 @@ public:
 
 	instruction(int8_t type = 255);
 	void load(int cur);
+	instruction &operator=(instruction &rhs) {
+		if (this == &rhs) return *this;
+		Ins_type = rhs.Ins_type;
+		imm = rhs.imm;
+		label = rhs.label;
+		offset = rhs.offset;
+		for (int i = 0; i < 3; ++i) regist[i] = rhs.regist[i];
+
+		return *this;
+	}
+	void clear() {
+		Ins_type = -1;
+		for (int i = 0; i < 3; ++i) regist[i] = EMPTY;
+		imm = label = offset = 0;
+	}
+};
+
+class IF : public instruction {
+public:
+	bool block = 0;
+	bool jump_block = 0;
+
+};
+
+class ID : public instruction {
+public:
+	bool block;
+	bool tovis[3];
+	int reg[3];
+
+	void push(IF &rhs) {
+		instruction *p = this;
+		*p = rhs;
+		p = &rhs;
+		p->clear();
+	}
+};
+
+class EX : public instruction {
+public:
+	int reg[3];
+	void push(ID &rhs) {
+		instruction *p = this;
+		*p = rhs;
+		p = &rhs;
+		p->clear();
+		for (int i = 0; i < 3; ++i) reg[i] = rhs.reg[i];
+	}
+};
+
+class MA : public instruction {
+public:
+	void push(EX &rhs) {
+		instruction *p = this;
+		*p = rhs;
+		p = &rhs;
+		p->clear();
+	}
+};
+
+class WB : public instruction {
+public:
+	void push(MA &rhs) {
+		instruction *p = this;
+		*p = rhs;
+		p = &rhs;
+		p->clear();
+	}
 };
